b4c287d53257e09a1324f68ca4b5c2ca55262aaa
zhaoxuan <zhaoxuan@alumni.tongji.edu.cn>
2020-04-23 01:12:05 +0800

    add .idea to ignore

--- src/main/java/com/zhaoxuan/learn/algorithm/Application.java
+++ src/main/java/com/zhaoxuan/learn/algorithm/Application.java
@@ -2,7 +2,7 @@ package com.zhaoxuan.learn.algorithm;
 
 /**
  * @author zhaoxuan
- * @description TODO
+ * @description 启动类
  * @createTime 2020/4/23 1:07 上午
  */
 public class Application {
---
+++ src/main/java/com/zhaoxuan/learn/algorithm/sparsearray/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author zhaoxuan
+ * @description TODO
+ * @createTime 2020/4/23 1:08 上午
+ */
+package com.zhaoxuan.learn.algorithm.sparsearray;
+
+/**
+ * 第二章 稀疏数组
+ *
+ */
